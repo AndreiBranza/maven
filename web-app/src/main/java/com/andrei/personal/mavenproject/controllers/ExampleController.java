b6e7f4aad69986014ae4ff9e9b54092db72d63fb
Andrei Branza <andrei.branza@yahoo.com>
2024-03-10 15:58:56 +0200

    Instantiated a json object from the added json file. Added the jsonschema2pojo plugin necessary for this instantiation.

--- web-app/src/main/java/com/andrei/personal/mavenproject/controllers/ExampleController.java
+++ web-app/src/main/java/com/andrei/personal/mavenproject/controllers/ExampleController.java
@@ -1,4 +1,17 @@
 package com.andrei.personal.mavenproject.controllers;
 
+import com.andrei.personal.model.User;
+import org.springframework.web.bind.annotation.RestController;
+import com.andrei.personal.json.model.ShippingAddress;
+@RestController
 public class ExampleController {
+
+    public ShippingAddress getShippingAddress() {
+
+        return new ShippingAddress();
+    }
+
+    public User getUser() {
+        return new User();
+    }
 }
