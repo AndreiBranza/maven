49ec8640aeae895307ff8c969f36b88c858cfbc2
Andrei Branza <andrei.branza@yahoo.com>
2024-03-10 19:12:12 +0200

    Added the junit4-testing module and copied some files fore complete directory structure

---
+++ junit4-testing/src/test/java/com/andrei/personal/TestMain.java
@@ -0,0 +1,16 @@
+package com.andrei.personal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestMain {
+
+    @Test
+    public void randomMethodName() {
+        Main main = new Main();
+
+        assertEquals("Hello world!", main.getHello());
+
+    }
+}
